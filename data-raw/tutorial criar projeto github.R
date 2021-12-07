c48f8609f53b20ffcae12fc597d2ee5ba1f3e145
Jessika <jangelottimendonca@gmail.com>
2021-12-07 20:48:52 -0300

    comentario

--- data-raw/tutorial criar projeto github.R
+++ data-raw/tutorial criar projeto github.R
@@ -1,8 +1,8 @@
 #Roteiro para criação de um projeto
 usethis::create_project("nome do meu projeto")
 #sempre cria no meus documentos, se não quiser pode especificar o path,
-#sendo que no final do path coloca o nome do projeto
-usethis::use_git() #somente executar
+#sendo que no final do path coloca o nome do projeto - usar sempre
+usethis::use_git() #somente executar - usar sempre
 
 #conectar o seu projeto a sua conta GitHub
 usethis::create_github_token()
@@ -10,4 +10,4 @@ usethis::create_github_token()
 #rodar
 gitcreds::gitcreds_set() #colar o token no console
 #crear pasta no github
-usethis::use_github()
+usethis::use_github() #usar sempre
